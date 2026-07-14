9c0492ef05a5dd3615b129344fff3c6045a73b0c
irone13 <ironyxzaoui@gmail.com>
2026-07-14 20:50:44 +0300

    Delete ex2.py

--- ex2.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Enter a number (0 to stop)"))
-number_of_try = 0
-while num !=0 :
-    num = int(input("Enter a number (0 to stop)"))
-    number_of_try +=1
-
-
-print ("0 was entered")
-print(number_of_try)
